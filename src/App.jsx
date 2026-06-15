37ff57a443e24d8408d5dc57c1a2dd038f57526f
scDjanGo <scdjango229101@gmail.com>
2026-06-15 16:16:20 +0600

    add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import ProductCard from "./components/cards/ProductCard";
 
-
-
-
-const BASE_URL = `https://69f8242bdd0c226688ee2bc2.mockapi.io/products`
-
-
-
+const BASE_URL = `https://69f8242bdd0c226688ee2bc2.mockapi.io/products/`;
 
 function App() {
-
-
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    fetchProducts();
+  }, []);
 
-
+  const fetchProducts = () => {
     fetch(BASE_URL)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      
-    })
-    .catch(err => {
-      console.log(err)
-    })
-
-
-    console.log("mount");
-    
-  }, [])
-
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
 
 
   return (
-    <div>App</div>
-  )
+    <div>
+    <button onClick={() => setProducts([])}>click</button>
+      {products.length ? (
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {products.map((item) => (
+            <ProductCard key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        "Empty"
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/cards/ProductCard.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+
+function ProductCard({ item }) {
+  
+
+
+
+  useEffect(() => {
+    return () => {
+      console.log("component did unmount");
+      
+    }
+  }, [])
+
+
+  return (
+      <div
+          key={item?.id}
+          className="overflow-hidden rounded-2xl border border-gray-200 bg-white shadow-sm"
+        >
+          <img
+            src={item?.image}
+            alt={item?.title}
+            className="h-56 w-full object-cover"
+          />
+
+          <div className="p-5">
+            <p className="mb-2 text-sm text-gray-500">
+              {new Date(item?.createdAt).toLocaleDateString()}
+            </p>
+
+            <h2 className="mb-3 text-xl font-semibold text-gray-900">
+              {item?.title}
+            </h2>
+
+            <p className="mb-4 text-sm text-gray-600">
+              {item?.description}
+            </p>
+
+            <span className="text-xs text-gray-400">
+              ID: {item?.id}
+            </span>
+          </div>
+        </div>
+  );
+}
+
+export default ProductCard;
